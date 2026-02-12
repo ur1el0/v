14819cda212c7a03160b8ceeabcd5da1aa346d7a
ur1el0 <zanoroosc@gmail.com>
2026-02-13 02:50:43 +0800

    second idea

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
-const noMessages = 
-[
-    "Are u sure?",
-    "Reallyyyy?",
-    "LUH TALAGA BAAAA",
-    "LANGLANG PLSSSS",
-    "Just think about it bb<333",
-    "ill be beri beri sad bb",
-    "FINE! IM NOT GONNA ASK ANYMORE!",
-    "Just kidding, say yes pls"
-];
-
 const yesMessages =
 [
-    "Are u sure babyboy",
+    "Are u sure babyboy?",
     "REALLYYYYYYY",
     "U SWEAR BABYY??"
 ]
@@ -24,10 +12,17 @@ const noImages =
     "https://i.giphy.com/ML1CuEfsj4CmI7jA7n.webp",
     "https://i.giphy.com/3o751ZN0aNKakEQdEY.webp",
 ]
+const yesImages =
+[
+    "https://i.giphy.com/4N1wOi78ZGzSB6H7vK.webp",
+    "https://i.giphy.com/jNJW9Bj6vVXIERUgK3.webp",
+    "https://i.giphy.com/qFmdpUKAFZ6rMobzzu.webp",
+    "https://i.giphy.com/7W1rgKAxlDe3m.webp"
+]
 
 let messageIndex = 0;
 let yesMessageIndex = 0;
-let i = 0;
+let gifIndex = 0;
 
 const yesButton = document.querySelector(".yes-button");
 const noButton = document.querySelector(".no-button");
@@ -40,13 +35,15 @@ yesButton.addEventListener("click", () => {
         head.textContent = yesMessages[yesMessageIndex];
         yesMessageIndex++;
         const currentSize = parseFloat(window.getComputedStyle(head).fontSize);
-        head.style.fontSize = `${currentSize * 1.8}px`;
+        head.style.fontSize = `${currentSize * 1.6}px`;
+        image.src = yesImages[gifIndex];
+        gifIndex = (gifIndex + 1) % yesImages.length;
     }
     else
     {
         head.textContent = "I knew u would say yes my langlang<3333";
         head.style.fontSize = "2.5em";
-        document.body.style.backgroundImage = `url('https://i.giphy.com/qFmdpUKAFZ6rMobzzu.webp')`;
+        document.body.style.backgroundImage = `url('https://i.giphy.com/qx3d4enif4rw3t5eng.webp')`;
         document.querySelector(".buttons").style.display = "none";
         yesButton.removeEventListener("click", arguments.callee);
         noButton.removeEventListener("click", arguments.callee);
@@ -54,20 +51,13 @@ yesButton.addEventListener("click", () => {
 });
 
 noButton.addEventListener("click", () => {
-    head.textContent = noMessages[messageIndex];
-    messageIndex = (messageIndex + 1) % noMessages.length;
-    const size = parseFloat(window.getComputedStyle(head).fontSize);
-    head.style.fontSize = `${size * 1.9}px`;
+    image.src = noImages[gifIndex];
+    gifIndex = (gifIndex + 1) % noImages.length;
+    const maxX = window.innerWidth - noButton.offsetWidth;
+    const maxY = window.innerHeight - noButton.offsetHeight;
+    const randomX = Math.random() * maxX;
+    const randomY = Math.random() * maxY;
+    noButton.style.position = 'fixed';
+    noButton.style.left = `${randomX}px`;
+    noButton.style.top = `${randomY}px`;
 });
-
-// https://i.giphy.com/W4RizlO6qZWQRYw9mb.webp 
-// https://i.giphy.com/lHTJgttLitH3BKpEXr.webp
-// https://i.giphy.com/ML1CuEfsj4CmI7jA7n.webp
-// https://i.giphy.com/3o751ZN0aNKakEQdEY.webp
-
-//remove image after 6 no's because it would take up the whole screen
-
-// use cornflower blue for background
-// HSL: \(218.5^{\circ },79.2\%,66.1\%\)
-
-// use hsl values for colors
